cba88dda5f625c2089c9945f39bf96693de24d4b
Liam <Liam@notmyemail.com>
2022-11-06 17:21:14 +0000

    add loading spinners

--- screens/JobPreviewScreen.tsx
+++ screens/JobPreviewScreen.tsx
@@ -1,4 +1,10 @@
-import { Pressable, ScrollView, FlatList, StyleSheet } from "react-native";
+import {
+  Pressable,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 import JobsPreview from "../components/JobPreview";
 import { Text, View } from "react-native";
 import { RootStackScreenProps, Job } from "../types";
@@ -10,56 +16,69 @@ export default function JobsPreviewScreen({
   navigation,
 }: RootStackScreenProps<"GO-SK">) {
   const emptyArr: Job[] = [];
-
   const [jobState, setJobState] = useState(emptyArr);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const getJobs = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch("https://go-sk.vercel.app/api/allJobs");
         const json = await res.json();
         const jobs: Job[] = json.props.jobs;
+        setIsLoading(false);
         return setJobState(jobs);
       } catch (err) {
         console.error(err);
+        setIsLoading(false);
       }
     };
 
     getJobs();
-  });
+  }, []);
 
   return (
     <>
-      <SafeAreaView className="w-full">
-        <FlatList
-          className=""
-          contentContainerStyle={{
-            paddingBottom: 50,
-          }}
-          data={jobState}
-          renderItem={({ item }) => (
-            <Pressable
-              onPress={() => {
-                navigation.navigate("Job", {
-                  job_id: item.job_id,
-                });
-              }}
-            >
-              <JobsPreview job={item} />
-            </Pressable>
-          )}
-          keyExtractor={(job) => job.job_id}
-          ListHeaderComponent={() => (
-            <View className="px-6  pb-9 w-full">
-              <View>
-                <Text className="text-5xl font-bold text-white">Jobs</Text>
-                <Text className="text-white text-xl">
-                  We partner with firms to get the best roles for people looking
-                  to relocate to Seoul.
-                </Text>
+      <SafeAreaView className="w-full flex-1">
+        {isLoading && (
+          <ActivityIndicator
+            size={"large"}
+            color={"white"}
+            className="justify-center flex-1"
+          />
+        )}
+        {!isLoading && (
+          <FlatList
+            className=""
+            contentContainerStyle={{
+              paddingBottom: 50,
+            }}
+            data={jobState}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => {
+                  navigation.navigate("Job", {
+                    job_id: item.job_id,
+                  });
+                }}
+              >
+                <JobsPreview job={item} />
+              </Pressable>
+            )}
+            keyExtractor={(job) => job.job_id}
+            ListHeaderComponent={() => (
+              <View className="px-6  pb-9 w-full">
+                <View>
+                  <Text className="text-5xl font-bold text-white">Jobs</Text>
+                  <Text className="text-white text-xl">
+                    We partner with firms to get the best roles for people
+                    looking to relocate to Seoul.
+                  </Text>
+                </View>
               </View>
-            </View>
-          )}
-        />
+            )}
+          />
+        )}
       </SafeAreaView>
     </>
   );
--- screens/JobScreen.tsx
+++ screens/JobScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
 import { jobs } from "../mockData";
 import { Job, RootStackScreenProps } from "../types";
 import RenderHtml, { HTMLSource } from "react-native-render-html";
@@ -20,6 +20,7 @@ export default function JobScreen({
     job_description: null,
   };
   const [jobState, setJobState] = useState(emptyJob);
+  const [isLoading, setIsLoading] = useState(true);
 
   const source = {
     html: `${jobState.job_description}`,
@@ -28,56 +29,68 @@ export default function JobScreen({
   useEffect(() => {
     const getJobById = async () => {
       try {
+        setIsLoading(true);
         const res = await fetch(
           `https://go-sk.vercel.app/api/jobs/${route.params.job_id}`
         );
         const json = await res.json();
         const job: Job = json.props.job;
+        setIsLoading(false);
         return setJobState(job);
       } catch (err) {
+        setIsLoading(false);
         console.error(err);
       }
     };
 
     getJobById();
-  });
+  }, []);
 
   const { width } = useWindowDimensions();
 
   return (
-    <SafeAreaView>
-      <ScrollView
-        contentContainerStyle={{
-          paddingBottom: 50,
-          paddingLeft: 15,
-          paddingRight: 15,
-        }}
-      >
-        <View className="px-2 pb-4">
-          <Text className="text-3xl text-white">{jobState.job_title}</Text>
-          <Text className="text-xl italic text-gray-400">
-            {jobState.location}
-          </Text>
-          <Text className="text-lg text-white mt-2">
-            {jobState.company_name}
-          </Text>
+    <SafeAreaView className="flex-1">
+      {isLoading && (
+        <View className="flex-1 justify-center">
+          <ActivityIndicator size={"large"} color={"white"} />
         </View>
-        <View className="mb-1 h-px w-full bg-gray-400" />
-        <RenderHtml
-          baseStyle={{
-            color: "white",
-            fontSize: 18,
-            width: "95%",
-            alignSelf: "center",
-            marginBottom: 10,
+      )}
+      {!isLoading && (
+        <ScrollView
+          contentContainerStyle={{
+            paddingBottom: 50,
+            paddingLeft: 15,
+            paddingRight: 15,
           }}
-          source={source}
-          contentWidth={width}
-          enableExperimentalMarginCollapsing={true}
-        />
-        <JobDetails job={jobState} />
-        <CustomButton buttonText="Apply" applyUrl={jobState.apply_url!} />
-      </ScrollView>
+        >
+          <View className="px-2 pb-4">
+            <Text className="text-3xl text-white">{jobState.job_title}</Text>
+            <Text className="text-xl italic text-gray-400">
+              {jobState.location}
+            </Text>
+            <Text className="text-lg text-white mt-2">
+              {jobState.company_name}
+            </Text>
+          </View>
+          <View className="mb-1 h-px w-full bg-gray-400" />
+          {jobState.job_description !== null && (
+            <RenderHtml
+              baseStyle={{
+                color: "white",
+                fontSize: 18,
+                width: "95%",
+                alignSelf: "center",
+                marginBottom: 10,
+              }}
+              source={source}
+              contentWidth={width}
+              // enableExperimentalMarginCollapsing={true}
+            />
+          )}
+          <JobDetails job={jobState} />
+          <CustomButton buttonText="Apply" applyUrl={jobState.apply_url!} />
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 }
